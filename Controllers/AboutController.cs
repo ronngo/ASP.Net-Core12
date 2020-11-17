d19b49e27ab054aa2485e69e497e56fcb2e718ad
ronngo <70925598+ronngo@users.noreply.github.com>
2020-11-17 18:15:48 +0700

    Add files via upload

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using tessrt.Models;
+
+namespace tessrt.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public AboutController(AppDbContext appDbContext, IWebHostEnvironment webHostEnvironment)
+        {
+            _appDbContext = appDbContext;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
